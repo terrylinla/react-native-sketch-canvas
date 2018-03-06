7c4109e269f6839f1269bcac0bc9864b1e9d3602
Sean Conrad <toblerpwn@gmail.com>
2018-03-06 13:45:11 -0800

    add `drawCurveFromPoints:inConext:` method

--- ios/RNSketchCanvas/RNSketchCanvas/RNSketchCanvasDelegate.m
+++ ios/RNSketchCanvas/RNSketchCanvas/RNSketchCanvasDelegate.m
@@ -10,10 +10,41 @@
 
 @implementation RNSketchCanvasDelegate
 
+CGPoint midPoint (CGPoint p1, CGPoint p2) {
+    return CGPointMake((p1.x + p2.x) * 0.5, (p1.y + p2.y) * 0.5);
+}
+
+- (void)drawCurveFromPoints:(NSArray *)points inContext:(CGContextRef)context {
+    for (int i = 0; i < points.count; i++) {
+        
+        CGPoint currentPoint = [points[i] CGPointValue];
+        CGPoint previousPoint = [points[i] CGPointValue];
+        CGPoint tertiaryPoint = [points[i] CGPointValue];
+        if (i == 1) {
+            previousPoint = [points[i - 1] CGPointValue];
+            tertiaryPoint = [points[i - 1] CGPointValue];
+        } else {
+            previousPoint = [points[i - 1] CGPointValue];
+            tertiaryPoint = [points[i - 2] CGPointValue];
+        }
+        
+        CGPoint mid1 = midPoint(previousPoint, tertiaryPoint);
+        CGPoint mid2 = midPoint(currentPoint, previousPoint);
+        CGContextMoveToPoint(context, mid1.x, mid1.y);
+        CGContextAddQuadCurveToPoint(context,
+                                     previousPoint.x,
+                                     previousPoint.y,
+                                     mid2.x,
+                                     mid2.y);
+        
+        CGContextStrokePath(context);
+        
+    }
+}
+
 - (void)drawLayer:(CALayer *)layer inContext:(CGContextRef)context {
     for (int index=0; index<_paths.count; index++) {
         RNSketchData *data = _paths[index];
-        bool first = true;
         CGContextSetLineWidth(context, (float)data.strokeWidth);
         CGContextSetLineCap(context, kCGLineCapRound);
         CGContextSetStrokeColorWithColor(context, [data.strokeColor CGColor]);
@@ -21,21 +52,16 @@
         if (data.cgPoints) {
             if (data.pointCount > 1) {
                 CGContextAddLines(context, data.cgPoints, data.pointCount);
+                CGContextStrokePath(context);
             } else if (data.pointCount == 1) {
                 CGPoint p = data.cgPoints[0];
                 CGContextMoveToPoint(context, p.x, p.y);
                 CGContextAddLineToPoint(context, p.x, p.y);
+                CGContextStrokePath(context);
             }
         } else {
-            for (int i=0; i<_currentPoints.count; i++) {
-                CGPoint point = [_currentPoints[i] CGPointValue];
-                if (first)  CGContextMoveToPoint(context, point.x, point.y);
-                first = false;
-                CGContextAddLineToPoint(context, point.x, point.y);
-            }
+            [self drawCurveFromPoints:_currentPoints inContext:context];
         }
-        
-        CGContextStrokePath(context);
     }
 }
 
