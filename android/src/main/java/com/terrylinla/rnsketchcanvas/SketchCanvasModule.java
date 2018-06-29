2ca2092381b975f4a614b29727499983d4fb22e8
TERRY <terry@TERRYde-MacBook-Pro.local>
2018-06-29 14:37:37 +0800

    fix #47

--- android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvasModule.java
+++ android/src/main/java/com/terrylinla/rnsketchcanvas/SketchCanvasModule.java
@@ -6,6 +6,9 @@ import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.uimanager.NativeViewHierarchyManager;
+import com.facebook.react.uimanager.UIBlock;
+import com.facebook.react.uimanager.UIManagerModule;
 
 public class SketchCanvasModule extends ReactContextBaseJavaModule {
     SketchCanvasModule(ReactApplicationContext reactContext) {
@@ -18,10 +21,17 @@ public class SketchCanvasModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void transferToBase64(int tag, String type, boolean transparent, Callback callback){
+    public void transferToBase64(final int tag, final String type, final boolean transparent, final Callback callback){
         try {
-            String base64 = SketchCanvasManager.Canvas.getBase64(type, transparent);
-            callback.invoke(null, base64);
+            final ReactApplicationContext context = getReactApplicationContext();
+            UIManagerModule uiManager = context.getNativeModule(UIManagerModule.class);
+            uiManager.addUIBlock(new UIBlock() {
+                public void execute(NativeViewHierarchyManager nvhm) {
+                    SketchCanvas view = (SketchCanvas) nvhm.resolveView(tag);
+                    String base64 = view.getBase64(type, transparent);
+                    callback.invoke(null, base64);
+                }
+            });
         } catch (Exception e) {
             callback.invoke(e.getMessage(), null);
         }
