305be73a88e2302403c5923bcb2f20e8bbb938c3
Thomas <thomas.steinbruechel@gmail.com>
2018-07-20 23:07:09 +0200

    1. Upgraded Gradle 2. Updated SDK Versions and made them overridable 3. Implement external storage permission request

--- example/android/app/src/main/java/com/example/MainApplication.java
+++ example/android/app/src/main/java/com/example/MainApplication.java
@@ -29,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
             new SketchCanvasPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
---
+++ example/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+class Example extends React.Component {
+  render() {
+    return <App />;
+  }
+}
+
+AppRegistry.registerComponent("Example", () => Example);
