18c731d5032a8ce2da96448d00881c8dedb19ec4
Jaime Bernardo <jaime@janeasystems.com>
2020-12-08 17:02:01 +0000

    example: Update to RN 0.63 and add Windows code

--- example/index.js
+++ example/index.js
@@ -1,11 +1,9 @@
-import React from "react";
-import { AppRegistry } from "react-native";
-import App from "./App";
+/**
+ * @format
+ */
 
-class Example extends React.Component {
-  render() {
-    return <App />;
-  }
-}
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-AppRegistry.registerComponent("Example", () => Example);
+AppRegistry.registerComponent(appName, () => App);
