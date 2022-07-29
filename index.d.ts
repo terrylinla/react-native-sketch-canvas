abf12921a005a0e8e70dde3d770687a169de4ca1
Mohd Nasrullah <56379244+dev-nasrullah@users.noreply.github.com>
2022-07-29 12:16:08 +0530

    error in type declaration
    error in type declaration file path should be filename

--- index.d.ts
+++ index.d.ts
@@ -55,7 +55,7 @@ export interface SavePreference {
 }
 
 export interface LocalSourceImage {
-  path: string
+  filename: string
   directory?: string
   mode?: 'AspectFill' | 'AspectFit' | 'ScaleToFill'
 }
